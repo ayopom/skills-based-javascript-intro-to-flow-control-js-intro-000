c554e96f0353a97e44f1198976e962c619a06402
Ayodele Pompey <ayodelepompey@gmail.com>
2019-02-26 19:22:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,6 @@
 function basicTeenager(age) {
 
-    if (age < 13 && age > 19) {
+    if (age >= 13 && age <= 19) {
       return "You are a teenager!";
     }
 
@@ -8,9 +8,9 @@ function basicTeenager(age) {
 
 function teenager(age) {
 
-      if (age < 13 && age > 19) {
-        return "You are a teenager!";
-      }
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
 
 }
 
